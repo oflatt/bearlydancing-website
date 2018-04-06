f1a22ba41190a6e780279c310387818d6fcf5d7a
Oliver <oflatt@gmail.com>
2018-04-06 11:07:19 -0600

    update font

--- generate-website.rkt
+++ generate-website.rkt
@@ -7,23 +7,32 @@
  `((html
     (head
      (title "bearly dancing")
-     (script (@ (src "mainscript.js"))))
+     (script (@ (src "mainscript.js")))
+     (link (@ (rel "stylesheet")
+              (type "text/css")
+              (href "style.css"))))
     (body
-     (@ (onload "changeButton()"))
+     (@ (onload "loadedHTML()")
+        (onresize "resizeBackground()")
+        (onclick "onClick(event)"))
      (canvas
       (@ (id "background")
          (style "position:fixed; top:0;left:0")
          (width "900")
          (height "900")))
-     (div
-      (@ (z-score "1")
-         (style "display: inline-block;position:relative"))
-      (button
-       (@
-        (id "download-button")
-        (style "height:60px;width:270px;font-size:15px")
-        (onclick "window.location.href='https://github.com/oflatt/bearlydancing/raw/master/src/pics/honeyback0.png'"))
-       "test button")))))
+     (center
+      (div
+       (@ (z-score "1")
+          (style "display: inline-block;position:relative"))
+       (center
+        (h1
+         "bearly dancing")
+        (button
+         (@
+          (id "download-button")
+          (style "height:60px;width:270px;background-color:black;outline-color:white")
+          (onclick "window.location.href='https://github.com/oflatt/bearlydancing/raw/master/src/pics/honeyback0.png'"))
+         "test button")))))))
  index-file-port)
 
 (close-output-port index-file-port)
--- mainscript.js
+++ mainscript.js
@@ -1,3 +1,14 @@
+var oldWindowWidth = null;
+var oldWindowHeight = null;
+
+function randInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function loadedHTML(){
+    changeButton();
+    resizeBackground();
+}
 
 function changeButton() {
     var button = document.getElementById('download-button');
@@ -10,4 +21,35 @@ function changeButton() {
 	button.setAttribute('onclick', "window.location.href='https://github.com/oflatt/bearlydancing/raw/master/src/pics/honeyback3.png'");
 	button.innerHTML = "Download (Windows)";
     }
+}
+
+function resizeBackground(){
+    var c=document.getElementById("background");
+    c.width = window.outerWidth;
+    c.height = window.outerHeight;
+    var ctx=c.getContext("2d");
+    ctx.fillStyle="rgb(0,0,0)";
+    ctx.fillRect(0,0,window.outerWidth, window.outerHeight);
+    oldWindowWidth = window.outerWidth;
+    oldWindowHeight = window.outerHeight;
+}
+
+function onKey(){
+    var c=document.getElementById("background");
+    var ctx=c.getContext("2d");
+    ctx.fillStyle="rgb(255,255,255)";
+    var pixel = 10;
+    ctx.fillRect(randInt(window.outerWidth/pixel)*pixel,randInt(window.outerHeight/pixel)*pixel,pixel,pixel);
+}
+
+window.onkeydown = onKey;
+
+function onClick(event) {
+    var c=document.getElementById("background");
+    var ctx=c.getContext("2d");
+    ctx.fillStyle="rgb(255,255,255)";
+    var pixel = 10;
+    var x = Math.floor(event.clientX/pixel)*pixel;
+    var y = Math.floor(event.clientY/pixel)*pixel;
+    ctx.fillRect(x, y,pixel,pixel);
 }
